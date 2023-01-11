cda2f91585d3d36627c69c2b8b9b2cdb848d067c
shady-mungai <shadrackmungai10@gmail.com>
2023-01-11 12:22:07 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ import { image } from "../data/data";
 function About() {
   return (
     <div id="about">
-    <h2>About me</h2>
-    <p> I am a React developer</p>
-    <img src = "{image}" alt="I made this"/>
-  </div>
+      <h2>About Me</h2>
+      <p> I am a React developer</p>
+      <img src ={image} alt="I made this"/>
+   </div>
   )
 }
 
